9f217e1de8100dba69cdc0ec867cc2928991ca4d
Hannes Braun <hannes.braun@ioki.com>
2024-02-14 17:06:31 +0100

    feat: introduce daterange filter
    Specified as target calendar for the filter method. Additionally makes
    sure to cast GTFS Enum columns to type Int64 and make sure that
    service_ids present in calendar_dates are also present in calendar.

--- gtfsutils/__init__.py
+++ gtfsutils/__init__.py
@@ -1,10 +1,11 @@
-import os
-import logging
 import datetime
+import logging
+import os
+from zipfile import ZipFile
+
+import geopandas as gpd
 import pandas as pd
 import shapely.geometry
-import geopandas as gpd
-from zipfile import ZipFile
 
 __version__ = "0.0.5"
 
@@ -12,15 +13,15 @@ logger = logging.getLogger(__name__)
 
 
 REQUIRED_GTFS_FILES = [
-    'agency',
-    'stops',
-    'routes',
-    'trips',
-    'calendar',
-    'stop_times',
-    #'shapes',
-    #'frequencies',
-    #'feedinfo'
+    "agency",
+    "stops",
+    "routes",
+    "trips",
+    "calendar",
+    "stop_times",
+    # 'shapes',
+    # 'frequencies',
+    # 'feedinfo'
 ]
 
 # https://developers.google.com/transit/gtfs/reference
@@ -41,119 +42,155 @@ AVAILABLE_GTFS_FILES = [
     "levels",
     "feed_info",
     "translations",
-    "attributions"
+    "attributions",
 ]
 
 ROUTE_TYPE_MAP = {
-    0: 'tram, light_rail',
-    1: 'subway',
-    2: 'rail, railway, train',
-    3: 'bus, ex-bus',
-    4: 'ferry',
-    5: 'cableCar',
-    6: 'gondola',
-    7: 'funicular'
+    0: "tram, light_rail",
+    1: "subway",
+    2: "rail, railway, train",
+    3: "bus, ex-bus",
+    4: "ferry",
+    5: "cableCar",
+    6: "gondola",
+    7: "funicular",
 }
 
 COLUMNS_DEPENDENCY_DICT = {
-    'agency':   ('agency_id',  ['routes']),
-    'routes':   ('route_id',   ['trips']),
-    'trips':    ('trip_id',    ['stop_times']),
-    'stops':    ('stop_id',    ['stop_times']),
-    'calendar': ('service_id', ['calendar_dates']),
-    'shapes':   ('shape_id',   ['trips'])
+    "agency": ("agency_id", ["routes"]),
+    "routes": ("route_id", ["trips"]),
+    "trips": ("trip_id", ["stop_times"]),
+    "stops": ("stop_id", ["stop_times"]),
+    "calendar": ("service_id", ["calendar_dates"]),
+    "shapes": ("shape_id", ["trips"]),
 }
+
+GTFS_ENUM_DATA_TYPES = {
+    "attributions": {"is_producer": "Int64"},
+    "calendar": {
+        "monday": "Int64",
+        "tuesday": "Int64",
+        "wednesday": "Int64",
+        "thursday": "Int64",
+        "friday": "Int64",
+        "saturday": "Int64",
+        "sunday": "Int64",
+    },
+    "calendar_dates": {"exception_type": "Int64"},
+    "routes": {
+        "route_type": "Int64",
+        "continuous_pickup": "Int64",
+        "continuous_drop_off": "Int64",
+    },
+    "stop_times": {
+        "pickup_type": "Int64",
+        "drop_off_type": "Int64",
+        "continuous_pickup": "Int64",
+        "continuous_drop_off": "Int64",
+        "timepoint": "Int64",
+    },
+    "stops": {"location_type": "Int64", "wheelchair_boarding": "Int64"},
+    "transfers": {"transfer_type": "Int64"},
+    "trips": {
+        "direction_id": "Int64",
+        "wheelchair_accessible": "Int64",
+        "bikes_allowed": "Int64",
+    },
+}
+
+
+def cast_gtfs_enum_columns(df_dict, filekey):
+    if filekey in GTFS_ENUM_DATA_TYPES.keys():
+        for column, dtype in GTFS_ENUM_DATA_TYPES[filekey].items():
+            if column in df_dict[filekey].columns:
+                df_dict[filekey][column] = df_dict[filekey][column].astype(dtype)
 
 
 def load_gtfs(filepath, subset=None):
     df_dict = {}
     if os.path.isdir(filepath):
         for filename in os.listdir(filepath):
-            filekey = filename.split('.txt')[0]
+            filekey = filename.split(".txt")[0]
             if (subset is None) or (filekey in subset):
                 try:
                     df_dict[filekey] = pd.read_csv(
-                        os.path.join(filepath, filename),
-                        low_memory=False)
+                        os.path.join(filepath, filename), low_memory=False
+                    )
+                    cast_gtfs_enum_columns(df_dict, filekey)
                 except Exception as e:
-                    logger.error(
-                        f"[{e.__class__.__name__}] {e} for {filename}")
+                    logger.error(f"[{e.__class__.__name__}] {e} for {filename}")
     else:
         with ZipFile(filepath) as z:
             for filename in z.namelist():
-                filekey = filename.split('.txt')[0]
+                filekey = filename.split(".txt")[0]
                 if (subset is None) or (filekey in subset):
                     try:
                         df_dict[filekey] = pd.read_csv(
-                            z.open(filename),
-                            low_memory=False)
+                            z.open(filename), low_memory=False
+                        )
+                        cast_gtfs_enum_columns(df_dict, filekey)
                     except Exception as e:
-                        logger.error(
-                            f"[{e.__class__.__name__}] {e} for {filename}")
-            
+                        logger.error(f"[{e.__class__.__name__}] {e} for {filename}")
+
     return df_dict
 
 
 def load_stops(src):
     if isinstance(src, str):
-        df_dict = load_gtfs(src, subset=['stops'])
+        df_dict = load_gtfs(src, subset=["stops"])
     elif isinstance(src, dict):
         df_dict = src
     else:
-        raise ValueError(
-            f"Data type not supported: {type(src)}")
+        raise ValueError(f"Data type not supported: {type(src)}")
 
-    stops = df_dict['stops'].copy()
+    stops = df_dict["stops"].copy()
     geoms = gpd.points_from_xy(stops.stop_lon, stops.stop_lat)
 
-    return gpd.GeoDataFrame(
-        stops, geometry=geoms, crs="EPSG:4326")
+    return gpd.GeoDataFrame(stops, geometry=geoms, crs="EPSG:4326")
 
 
-def load_shapes(src, geom_type='linestring'):
+def load_shapes(src, geom_type="linestring"):
     if isinstance(src, str):
-        df_dict = load_gtfs(src, subset=['shapes'])
+        df_dict = load_gtfs(src, subset=["shapes"])
     elif isinstance(src, dict):
         df_dict = src
     else:
-        raise ValueError(
-            f"Data type not supported: {type(src)}")
-    
-    if 'shapes' not in df_dict:
+        raise ValueError(f"Data type not supported: {type(src)}")
+
+    if "shapes" not in df_dict:
         raise ValueError("shapes.txt not found in GTFS")
 
-    if geom_type == 'linestring':
+    if geom_type == "linestring":
         items = []
-        for shape_id, g in df_dict['shapes'].groupby('shape_id'):
-            g = g.sort_values('shape_pt_sequence')
-            coords = g[['shape_pt_lon', 'shape_pt_lat']].values
+        for shape_id, g in df_dict["shapes"].groupby("shape_id"):
+            g = g.sort_values("shape_pt_sequence")
+            coords = g[["shape_pt_lon", "shape_pt_lat"]].values
 
             if len(coords) > 1:
-                items.append({
-                    'shape_id': shape_id,
-                    'geom': shapely.geometry.LineString(coords)
-                })
+                items.append(
+                    {"shape_id": shape_id, "geom": shapely.geometry.LineString(coords)}
+                )
 
-        gdf = gpd.GeoDataFrame(items, geometry='geom', crs="EPSG:4326")
+        gdf = gpd.GeoDataFrame(items, geometry="geom", crs="EPSG:4326")
 
-    elif geom_type == 'point':
-        shapes = df_dict['shapes'].copy()
+    elif geom_type == "point":
+        shapes = df_dict["shapes"].copy()
         geoms = gpd.points_from_xy(shapes.shape_pt_lon, shapes.shape_pt_lat)
 
         gdf = gpd.GeoDataFrame(shapes, geometry=geoms, crs="EPSG:4326")
 
     else:
-        raise ValueError(
-            f"Geometry type {geom_type} not supported!")
+        raise ValueError(f"Geometry type {geom_type} not supported!")
 
     return gdf
 
 
 def save_gtfs(df_dict, filepath, ignore_required=False, overwrite=False):
-    if not ignore_required and not all(key in df_dict.keys() 
-               for key in REQUIRED_GTFS_FILES):
+    if not ignore_required and not all(
+        key in df_dict.keys() for key in REQUIRED_GTFS_FILES
+    ):
         raise ValueError("Not all required GTFS files in dictionary")
-    
+
     if overwrite or not os.path.exists(filepath):
         with ZipFile(filepath, "w") as zf:
             for filekey, df in df_dict.items():
@@ -163,42 +200,42 @@ def save_gtfs(df_dict, filepath, ignore_required=False, overwrite=False):
 
 def get_bounding_box(src):
     if isinstance(src, str):
-        df_dict = load_gtfs(src, subset=['stops'])
+        df_dict = load_gtfs(src, subset=["stops"])
     elif isinstance(src, dict):
         df_dict = src
     else:
-        raise ValueError(
-            f"Data type not supported: {type(src)}")
+        raise ValueError(f"Data type not supported: {type(src)}")
 
     return [
-        df_dict['stops']['stop_lon'].min(),
-        df_dict['stops']['stop_lat'].min(),
-        df_dict['stops']['stop_lon'].max(),
-        df_dict['stops']['stop_lat'].max()
+        df_dict["stops"]["stop_lon"].min(),
+        df_dict["stops"]["stop_lat"].min(),
+        df_dict["stops"]["stop_lon"].max(),
+        df_dict["stops"]["stop_lat"].max(),
     ]
 
 
 def get_calendar_date_range(src):
     if isinstance(src, str):
-        df_dict = load_gtfs(src, subset=['calendar'])
+        df_dict = load_gtfs(src, subset=["calendar"])
     elif isinstance(src, dict):
         df_dict = src
     else:
-        raise ValueError(
-            f"Data type not supported: {type(src)}")
+        raise ValueError(f"Data type not supported: {type(src)}")
 
     if "calendar" in df_dict:
         min_date = min(
-            df_dict['calendar']['start_date'].min(),
-            df_dict['calendar']['end_date'].min())
+            df_dict["calendar"]["start_date"].min(),
+            df_dict["calendar"]["end_date"].min(),
+        )
         max_date = max(
-            df_dict['calendar']['start_date'].max(),
-            df_dict['calendar']['end_date'].max())
+            df_dict["calendar"]["start_date"].max(),
+            df_dict["calendar"]["end_date"].max(),
+        )
         min_date = datetime.datetime.strptime(str(min_date), "%Y%m%d")
         max_date = datetime.datetime.strptime(str(max_date), "%Y%m%d")
     else:
         raise ValueError("calendar.txt missing")
-    
+
     return min_date, max_date
 
 
@@ -208,17 +245,18 @@ def print_info(src):
     elif isinstance(src, dict):
         df_dict = src
     else:
-        raise ValueError(
-            f"Data type not supported: {type(src)}")
+        raise ValueError(f"Data type not supported: {type(src)}")
 
     print("\nGTFS files:")
     for key in sorted(df_dict.keys()):
         print(f"  {key + '.txt':<20s} {len(df_dict[key]):12,d} rows")
 
     min_date, max_date = get_calendar_date_range(df_dict)
-    print("\nCalender date range:\n  " \
-        f"{min_date.strftime('%d.%m.%Y')} - "\
-        f"{max_date.strftime('%d.%m.%Y')}")
+    print(
+        "\nCalender date range:\n  "
+        f"{min_date.strftime('%d.%m.%Y')} - "
+        f"{max_date.strftime('%d.%m.%Y')}"
+    )
 
     bounds = get_bounding_box(df_dict)
     print(f"\nBounding box:\n  {bounds}\n")
--- gtfsutils/__main__.py
+++ gtfsutils/__main__.py
@@ -1,23 +1,15 @@
-import json
-import time
-import logging
 import argparse
-import geopandas as gpd
+import json
+import logging
+import time
 from datetime import datetime
+
+import geopandas as gpd
 
 import gtfsutils
 import gtfsutils.filter
 
 logger = logging.getLogger(__name__)
-
-
-def prepare_bounds(bounds):
-    if bounds.startswith("["):
-        return json.loads(bounds)
-    else:
-        return gpd.read_file(bounds).geometry.unary_union
-
-
 
 
 def main():
@@ -28,30 +20,37 @@ def main():
     parser_filter = subparsers.add_parser("filter", help="Filter method")
     parser_filter.add_argument(dest="src", help="Input GTFS filepath")
     parser_filter.add_argument(dest="dst", help="Output GTFS filepath")
-    parser_filter.add_argument("-b", "--bounds",
+    parser_filter.add_argument(
         dest="bounds",
-        help="Filter boundary")
-    parser_filter.add_argument("-t", "--target",
-        dest='target',
+        help="""Filter boundary (Filepath to GeoJSON file, boundaries \
+        ('[16.197, 47.999, 16.549, 48.301]') or start date and end date of a daterange \
+        ('{"start_date": "YYYYMMDD", "end_date": "YYYYMMDD"}')""",
+    )
+    parser_filter.add_argument(
+        "-t",
+        "--target",
+        dest="target",
         help="Filter target (stops, shapes, calendar)",
-        default="stops")
-    parser_filter.add_argument("-o", "--operation",
-        dest='operation',
+        default="stops",
+    )
+    parser_filter.add_argument(
+        "-o",
+        "--operation",
+        dest="operation",
         help="Filter operation (within, intersects)",
-        default="within")
-    parser_filter.add_argument("-sd", "--start_date",
-        dest="start_date",
-        help="Filter start date (ISO 8601 format: e.g. '20240213')",
-        default=None)
-    parser_filter.add_argument("-ed", "--end_date",
-        dest="end_date",
-        help="Filter end date (ISO 8601 format: e.g. '20240213')",
-        default=None)
-    parser_filter.add_argument("--overwrite", action='store_true',
-        dest='overwrite', help="Overwrite if exists")
-    parser_filter.add_argument('-v', '--verbose', action='store_true',
-        dest='verbose', default=False,
-        help="Verbose output")
+        default="within",
+    )
+    parser_filter.add_argument(
+        "--overwrite", action="store_true", dest="overwrite", help="Overwrite if exists"
+    )
+    parser_filter.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        default=False,
+        help="Verbose output",
+    )
 
     # Bounds method
     parser_bounds = subparsers.add_parser("bounds", help="Bounds method")
@@ -67,12 +66,13 @@ def main():
     args = parser.parse_args()
 
     # # Verbose output
-    if 'verbose' in args:
+    if "verbose" in args:
         log_level = logging.DEBUG if args.verbose else logging.INFO
         logging.basicConfig(
-            format='%(asctime)s [%(levelname)s] %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S',
-            level=log_level)
+            format="%(asctime)s [%(levelname)s] %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+            level=log_level,
+        )
 
     if args.method == "version":
         print(f"{gtfsutils.__name__} {gtfsutils.__version__}")
@@ -80,58 +80,56 @@ def main():
     elif args.method == "filter":
         assert args.src is not None, "No input file specified"
         assert args.dst is not None, "No output file specified"
+        assert args.bounds is not None, "No bounds defined"
 
-        if args.target in ["stops", "shapes"]:
-            assert args.bounds is not None, "No bounds defined"
-
-        elif args.target == "calendar":
-            assert args.start_date is not None, "No start date defined"
-            assert args.end_date is not None, "No end date defined"
+        # Prepare bounds
+        if args.bounds.startswith("["):
+            bounds = json.loads(args.bounds)
+        elif args.bounds.startswith("{"):
+            bounds = json.loads(args.bounds)
+            start_date = datetime.strptime(bounds["start_date"], "%Y%m%d")
+            end_date = datetime.strptime(bounds["end_date"], "%Y%m%d")
+        else:
+            bounds = gpd.read_file(args.bounds).geometry.unary_union
 
         # Load GTFS
         t = time.time()
+        logger.debug(f"Start loading {args.src}")
         df_dict = gtfsutils.load_gtfs(args.src)
         duration = time.time() - t
         logger.debug(f"Loaded {args.src} in {duration:.2f}s")
 
         # Filter GTFS
-        if args.target == 'stops':
-            bounds = prepare_bounds(args.bounds)
+        if args.target == "stops":
             t = time.time()
-            gtfsutils.filter.spatial_filter_by_stops(
-                df_dict, bounds)
-        elif args.target == 'shapes':
-            bounds = prepare_bounds(args.bounds)
+            gtfsutils.filter.spatial_filter_by_stops(df_dict, bounds)
+        elif args.target == "shapes":
             t = time.time()
             gtfsutils.filter.spatial_filter_by_shapes(
-                df_dict, bounds, operation=args.operation)
-        elif args.target == 'calendar':
-            # Prepare start_date and end_date
-            start_date = datetime.strptime(args.start_date, "%Y%m%d")
-            end_date = datetime.strptime(args.end_date, "%Y%m%d")
+                df_dict, bounds, operation=args.operation
+            )
+        elif args.target == "calendar":
             t = time.time()
-            gtfsutils.filter.filter_by_calendar(
-                df_dict, start_date, end_date)
-            import sys
-            sys.exit()
+            gtfsutils.filter.filter_by_calendar(df_dict, start_date, end_date)
         else:
-            raise ValueError(
-                f"Target {args.target} not supported!")
+            raise ValueError(f"Target {args.target} not supported!")
         duration = time.time() - t
         logger.debug(f"Filtered {args.src} in {duration:.2f}s")
 
         # Save filtered GTFS
         t = time.time()
-        gtfsutils.save_gtfs(df_dict, args.dst, ignore_required=True, overwrite=args.overwrite)
+        gtfsutils.save_gtfs(
+            df_dict, args.dst, ignore_required=True, overwrite=args.overwrite
+        )
         duration = time.time() - t
         logger.debug(f"Saved to {args.dst} in {duration:.2f}s")
-    
+
     elif args.method == "bounds":
         assert args.src is not None, "No input file specified"
 
         bounds = gtfsutils.get_bounding_box(args.src)
         print(bounds)
-    
+
     elif args.method == "info":
         assert args.src is not None, "No input file specified"
         gtfsutils.print_info(args.src)
@@ -140,5 +138,5 @@ def main():
         raise NotImplementedError("Merge not implemented")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

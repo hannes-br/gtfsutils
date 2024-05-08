bf595efe0019f3e830c8a98c578db482aa44d328
Hannes <45296070+hannes-br@users.noreply.github.com>
2024-05-08 16:42:41 +0200

    feat: make sure gtfs ids are always of type string (#5)

--- tests/test_gtfsutils.py
+++ tests/test_gtfsutils.py
@@ -1,6 +1,8 @@
 import datetime
+
 import numpy as np
 import pandas as pd
+
 import gtfsutils
 
 
@@ -29,6 +31,29 @@ def test__gtfsutils_get_calendar_date_range():
     filepath = "tests/data/sample-feed.zip"
     min_data, max_date = gtfsutils.get_calendar_date_range(filepath)
 
-    assert isinstance(min_data, datetime.datetime) and \
-           isinstance(max_date, datetime.datetime)
+    assert isinstance(min_data, datetime.datetime) and isinstance(
+        max_date, datetime.datetime
+    )
     assert min_data <= max_date
+
+
+def test__cast_gtfs_file_ids():
+    filepath = "tests/data/sample-feed.zip"
+    df_dict = gtfsutils.load_gtfs(filepath)
+    gtfsutils.cast_gtfs_ids(df_dict, "agency")
+    gtfsutils.cast_gtfs_ids(df_dict, "routes")
+    gtfsutils.cast_gtfs_ids(df_dict, "trips")
+    gtfsutils.cast_gtfs_ids(df_dict, "calendar")
+    gtfsutils.cast_gtfs_ids(df_dict, "stops")
+    gtfsutils.cast_gtfs_ids(df_dict, "shapes")
+
+    assert isinstance(df_dict["agency"]["agency_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["routes"]["agency_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["routes"]["route_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["trips"]["route_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["trips"]["service_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["trips"]["shape_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["trips"]["trip_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["calendar"]["service_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["stops"]["stop_id"].dtype, pd.StringDtype)
+    assert isinstance(df_dict["shapes"]["shape_id"].dtype, pd.StringDtype)

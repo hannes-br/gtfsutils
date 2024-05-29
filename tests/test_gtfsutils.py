700da69dbe33dee98d75269dc2408e48b25350e3
hannes.braun <hannesbraun92@gmail.com>
2024-05-29 11:59:47 +0200

    feat: add spatial filter target stations
    In contrast to spatial filter target stops, this filter returns complete
    stations with all of their respective child stops if at least one of the
    stops or the station itself is located within the filter boundary.

--- tests/test_gtfsutils.py
+++ tests/test_gtfsutils.py
@@ -1,16 +1,19 @@
 import datetime
 
+import geopandas as gpd
 import numpy as np
 import pandas as pd
 
 import gtfsutils
+import gtfsutils.filter
+
+GTFS_SAMPLE_FEED = "tests/data/full_greater_sydney_gtfs_parent_tsn_1.zip"
 
 
 def test__gtfsutils_load_gtfs():
     # Sample feed from:
-    # https://developers.google.com/transit/gtfs/examples/gtfs-feed
-    # https://developers.google.com/static/transit/gtfs/examples/sample-feed.zip
-    filepath = "tests/data/sample-feed.zip"
+    # https://opendata.transport.nsw.gov.au/dataset/temporary-andor-sample-gtfs-data/resource/5fc873b6-85d6-4c39-bdd5-6c3a102a703f
+    filepath = GTFS_SAMPLE_FEED
     df_dict = gtfsutils.load_gtfs(filepath)
 
     assert isinstance(df_dict, dict)
@@ -19,7 +22,7 @@ def test__gtfsutils_load_gtfs():
 
 
 def test__gtfsutils_get_bounding_box():
-    filepath = "tests/data/sample-feed.zip"
+    filepath = GTFS_SAMPLE_FEED
     bbox = gtfsutils.get_bounding_box(filepath)
 
     assert isinstance(bbox, list) or isinstance(bbox, np.ndarray)
@@ -28,7 +31,7 @@ def test__gtfsutils_get_bounding_box():
 
 
 def test__gtfsutils_get_calendar_date_range():
-    filepath = "tests/data/sample-feed.zip"
+    filepath = GTFS_SAMPLE_FEED
     min_data, max_date = gtfsutils.get_calendar_date_range(filepath)
 
     assert isinstance(min_data, datetime.datetime) and isinstance(
@@ -38,7 +41,7 @@ def test__gtfsutils_get_calendar_date_range():
 
 
 def test__cast_gtfs_file_ids():
-    filepath = "tests/data/sample-feed.zip"
+    filepath = GTFS_SAMPLE_FEED
     df_dict = gtfsutils.load_gtfs(filepath)
     gtfsutils.cast_gtfs_ids(df_dict, "agency")
     gtfsutils.cast_gtfs_ids(df_dict, "routes")
@@ -57,3 +60,65 @@ def test__cast_gtfs_file_ids():
     assert isinstance(df_dict["calendar"]["service_id"].dtype, pd.StringDtype)
     assert isinstance(df_dict["stops"]["stop_id"].dtype, pd.StringDtype)
     assert isinstance(df_dict["shapes"]["shape_id"].dtype, pd.StringDtype)
+
+
+def test__get_stop_ids_including_stations_within_geometry():
+    filepath = GTFS_SAMPLE_FEED
+    df_dict = gtfsutils.load_gtfs(filepath)
+    bounds = gpd.read_file("tests/data/sydney_subset.geojson").geometry.unary_union
+
+    stop_ids = gtfsutils.filter.get_stop_ids_including_stations_within_geometry(
+        df_dict, bounds
+    )
+    assert len(stop_ids) == 47
+    assert sorted(stop_ids) == sorted(
+        [
+            "200020",
+            "2000147",
+            "20002",
+            "200020.0",
+            "200020_CNC1",
+            "200020_CNC2",
+            "200020_DP1",
+            "200020_DP10",
+            "200020_DP11",
+            "200020_DP12",
+            "200020_DP13",
+            "200020_DP14",
+            "200020_DP15",
+            "200020_DP2",
+            "200020_DP3",
+            "200020_DP4",
+            "200020_DP5",
+            "200020_DP6",
+            "200020_DP7",
+            "200020_DP8",
+            "200020_DP9",
+            "200020_ENT1",
+            "200020_ENT2",
+            "200020_ENT3",
+            "200020_ENT4",
+            "200020_ENT5",
+            "2000212",
+            "2000215",
+            "2000216",
+            "2000217",
+            "2000223",
+            "2000225",
+            "2000227",
+            "2000229",
+            "2000234",
+            "2000235",
+            "2000274",
+            "20003",
+            "2000351",
+            "2000352",
+            "20004",
+            "2000449",
+            "2000450",
+            "2000451",
+            "20005",
+            "20006",
+            "200065",
+        ]
+    )
